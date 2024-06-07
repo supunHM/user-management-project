bfa7f05d42fcbfc06dba10cf9880a808eb364325
supunHMSM <152811648+supunHM@users.noreply.github.com>
2024-06-07 14:36:56 +0530

    setup object array

--- example.java
+++ example.java
@@ -1,6 +1,71 @@
 
 import java.time.*;
 import java.util.*;
+
+class Customer{
+	private String id;
+	private String name;
+	private String phoneNumber;
+	private String companyName;
+	private double salary;
+	private String birthday;
+	//-------Constructer--------
+	Customer(String id, String name , String phoneNumber , String companyName , double salary , String birthday){
+		this.id = id;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.companyName = companyName;
+		this.salary = salary;
+		this.birthday = birthday;
+	}
+
+	//---------Setters----------------------
+	public void setId(String id){
+		this.id = id;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public void setPhoneNumber(String phoneNumber){
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void setCompanyName(String companyName){
+		this.companyName = companyName;
+	}
+
+	public void setBirthday(double salary){
+		this.salary = salary;
+	}
+
+	public void setBirthday(String birthday){
+		this.birthday = birthday;
+	}
+
+	//----------Getters--------------
+
+	public String getId(){
+		return id;
+	}
+	public String getName(){
+		return name;
+	}
+	public String getPhoneNumber(){
+		return phoneNumber;
+	}
+	public String getCompanyName(){
+		return companyName;
+	}
+	public double getSalary(){
+		return salary;
+	}
+	public String getBirthday(){
+		return birthday;
+	}
+
+}
 class example{
     //-------------------CREATE AN ARRAYS ----------------
     public static String[] idArray = new String[0];
